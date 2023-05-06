531a2c4dfc47538b4c2e53b7d72c0c64108e7db9
José Balboa <joselimberg291197@gmail.com>
2023-05-06 12:21:55 -0700

    added link function

--- pages/qrcodereader.tsx
+++ pages/qrcodereader.tsx
@@ -1,45 +1,56 @@
 import { useState } from "react";
 import { QrReader } from "react-qr-reader";
 import { QRLayout } from "../components/layouts/QRLayout";
+import { useRouter } from "next/router";
 
 export default function QRCodeReader() {
+  const router = useRouter();
   const [text, setText] = useState("Escaneando...");
+  const [showButtonR, setShowButtonR] = useState(false);
 
   return (
     <QRLayout pageDescription="Lector QR" title="Lector QR">
       <div>
-      <h1 className="text-5xl text-center">Lector de QR</h1>
-      <div className="flex justify-center">
-        <QrReader
-          onResult={(result, error) => {
-            if (!!result) {
-              setText((result as any).text);
-            }
+        <h1 className="text-5xl text-center">Lector de QR</h1>
+        <div className="flex justify-center">
+          <QrReader
+            onResult={(result, error) => {
+              if (!!result) {
+                setText((result as any).text);
+                if (text !== "Escaneando...") {
+                  setShowButtonR(true);
+                }
+              }
 
-            if (!!error) {
-              console.log(error);
-              // setError(error as any);
-            }
-          }}
-          constraints={{ facingMode: "environment" }}
-          scanDelay={500}
-          className="border"
-          containerStyle={{width:"256px", heigth:"256px"}}
-          videoStyle={{width:"256px", heigth:"256px"}}
-          videoContainerStyle={{width:"256px", heigth:"256px"}}
-          ViewFinder={() => {
-            return (<div className="">soy el overlay</div>)
-          }}
-          videoId="qrvideo"
-
-        />
-
-        
+              if (!!error) {
+                console.log(error);
+                // setError(error as any);
+              }
+            }}
+            constraints={{ facingMode: "environment" }}
+            scanDelay={500}
+            className="border"
+            containerStyle={{ width: "256px", heigth: "256px" }}
+            videoStyle={{ width: "256px", heigth: "256px" }}
+            videoContainerStyle={{ width: "256px", heigth: "256px" }}
+            ViewFinder={() => {
+              return <div className="">soy el overlay</div>;
+            }}
+            videoId="qrvideo"
+          />
+        </div>
+        <div className="flex justify-center">
+          <h1 className="text-2xl">{text}</h1>
+        </div>
+        <div className="flex justify-center">
+          <button 
+          className={` bg-sky-700 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded ${showButtonR ? 'block' : 'hidden'}`}
+            onClick={() => {
+              router.push(text);
+            }}
+          >Ir al enlace</button>
+        </div>
       </div>
-      <div className="flex justify-center"><h1 className="text-2xl">{text}</h1></div>
-      
-    </div>
     </QRLayout>
-    
   );
 }
